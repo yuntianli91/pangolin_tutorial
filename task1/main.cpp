61779331e3f0556b1b036811694ff66638aafb2e
yuntian <yuntianlee91@hotmail.com>
2020-03-26 20:01:40 +0800

    upload task1

--- task1/main.cpp
+++ task1/main.cpp
@@ -4,20 +4,16 @@ int main( int /*argc*/, char** /*argv*/ )
 {
     // 创建名称为“Main”的GUI窗口，尺寸为640×640
     pangolin::CreateWindowAndBind("Main",640,480);
-    // 启动深度测试(即关闭透视，只会看到最前面的物体，否则将会看到后面的物体)
+    // 启动深度测试
     glEnable(GL_DEPTH_TEST);
 
     // 创建一个观察相机视图
-    // ProjectMatrix(int h, int w, int fu, int fv, int cu, int cv, int znear, int zfar) 
-    //      参数依次为观察相机的图像高度、宽度、4个内参以及最近和最远视距
-    // ModelViewLookAt(double x, double y, double z,double lx, double ly, double lz, AxisDirection Up)
-    //      参数一次为相机所在的位置，以及相机所看的视点位置(一般会设置在原点)
     pangolin::OpenGlRenderState s_cam(
         pangolin::ProjectionMatrix(640,480,420,420,320,320,0.2,100),
         pangolin::ModelViewLookAt(2,0,2, 0,0,0, pangolin::AxisY)
     );
 
-    // 创建交互视窗
+    // 创建交互视图
     pangolin::Handler3D handler(s_cam); //交互相机视图句柄
     pangolin::View& d_cam = pangolin::CreateDisplay()
             .SetBounds(0.0, 1.0, 0.0, 1.0, -640.0f/480.0f)
@@ -45,7 +41,7 @@ int main( int /*argc*/, char** /*argv*/ )
 	    glVertex3f( -1,-1,0 );
 	    glEnd();
 
-        // Swap frames and Process Events
+        // 运行帧循环以推进窗口事件
         pangolin::FinishFrame();
     }
     
